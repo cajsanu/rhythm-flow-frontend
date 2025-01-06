688ffdfd5e412c8a534a2a346cc1d3c79ae0f387
Cajsa Nummelin <cajsa.nummelin@gmail.com>
2025-01-06 11:03:44 +0200

    initialise project

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,25 @@
+import React from "react"
 import ReactDOM from "react-dom/client"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
-import App from "./App"
 import "./index.css"
+import { Home } from "./pages/home"
+
+const router = createBrowserRouter([
+  {
+    path: "/home",
+    element: <Home />,
+    errorElement: <div>"Not found"</div>
+  }
+])
 
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <QueryClientProvider client={queryClient}>
-    <App />
-  </QueryClientProvider>
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </React.StrictMode>
 )
